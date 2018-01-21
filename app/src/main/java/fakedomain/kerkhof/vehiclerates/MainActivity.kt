b4a18770677fa3ef25ac7fbf6b6a88f143ec2a04
Tyler Kerkhof <kerkhof.tyler@gmail.com>
2018-01-21 17:00:23 -0600

    Added buttons for testing the web services

--- app/src/main/java/fakedomain/kerkhof/vehiclerates/MainActivity.kt
+++ app/src/main/java/fakedomain/kerkhof/vehiclerates/MainActivity.kt
@@ -2,11 +2,26 @@ package fakedomain.kerkhof.vehiclerates
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var postButton = findViewById<Button>(R.id.post_button)
+        var getButton = findViewById<Button>(R.id.get_button)
+
+        postButton.setOnClickListener { testMethod1() }
+        getButton.setOnClickListener {  testMethod2() }
+    }
+
+    private fun testMethod1() {
+        var cat = 0;
+    }
+
+    private fun testMethod2() {
+        var cat = 2;
     }
 }
