7e77daf183d39053c8d2ad40cdbbe52d16351906
Tyler Kerkhof <kerkhof.tyler@gmail.com>
2018-01-21 18:16:36 -0600

    Got a response from the Get Vehicle Rates service

--- app/src/main/java/fakedomain/kerkhof/vehiclerates/MainActivity.kt
+++ app/src/main/java/fakedomain/kerkhof/vehiclerates/MainActivity.kt
@@ -3,25 +3,39 @@ package fakedomain.kerkhof.vehiclerates
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import fakedomain.kerkhof.vehiclerates.helpers.Constants
+import okhttp3.*
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
+
+    private val client = OkHttpClient()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var postButton = findViewById<Button>(R.id.post_button)
-        var getButton = findViewById<Button>(R.id.get_button)
+        val postButton = findViewById<Button>(R.id.post_button)
+        val getButton = findViewById<Button>(R.id.get_button)
 
-        postButton.setOnClickListener { testMethod1() }
-        getButton.setOnClickListener {  testMethod2() }
+        postButton.setOnClickListener { getVehicleRates() }
+        getButton.setOnClickListener {  createVehicleRate() }
     }
 
-    private fun testMethod1() {
-        var cat = 0;
+    private fun getVehicleRates() {
+        val authHeader = Credentials.basic(Constants.AUTH_USERNAME, Constants.AUTH_PASS)
+        val request = Request.Builder()
+                .url(Constants.URL_VEHICLE_RATES)
+                .header(Constants.AUTH_NAME, authHeader)
+                .build()
+
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {}
+            override fun onResponse(call: Call, response: Response) = println(response.body()?.string())
+        })
     }
 
-    private fun testMethod2() {
-        var cat = 2;
+    private fun createVehicleRate() {
+        // TODO: write create vehicle rate service
     }
 }
---
+++ app/src/main/java/fakedomain/kerkhof/vehiclerates/helpers/Constants.kt
@@ -0,0 +1,11 @@
+package fakedomain.kerkhof.vehiclerates.helpers
+
+/**
+ * Created by tkerkhof on 1/21/18.
+ */
+object Constants {
+    const val URL_VEHICLE_RATES = "https://go-denver-staging.eliteextra.com/x/api/vehiclerates"
+    const val AUTH_NAME = "Authorization"
+    const val AUTH_USERNAME = "adc-developer-test"
+    const val AUTH_PASS = "2534f4ddaabb6a126dc84645a181ae85"
+}
